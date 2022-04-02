668bfc1e17d06e450e95367408e0497db0047590
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-04-02 09:57:51 -0400

    Updated Player and State class Player has 2 types, computer and human State has 2 states depending on whose turn it is Over or Ongoing.

--- src/main/scala/org/mypackage/domain/Player.scala
+++ src/main/scala/org/mypackage/domain/Player.scala
@@ -1,5 +1,7 @@
 package org.mypackage.domain
 
+sealed trait Player
 object Player {
-
+  final case object Computer extends Player
+  final case object Human    extends Player
 }
--- src/main/scala/org/mypackage/domain/State.scala
+++ src/main/scala/org/mypackage/domain/State.scala
@@ -1,5 +1,13 @@
 package org.mypackage.domain
+sealed trait State { self =>
+  def isComputerTurn: Boolean = self match {
+    case State.Ongoing(_, whoIsCross, turn) =>
+      (turn == Piece.X && whoIsCross == Player.Computer) || (turn == Piece.O && whoIsCross == Player.Human)
+    case State.Over(_) => false
+  }
 
+}
 object State {
-
+  final case class Ongoing(board: Board, whoIsCross: Player, turn: Piece) extends State
+  final case class Over(board: Board)
 }
