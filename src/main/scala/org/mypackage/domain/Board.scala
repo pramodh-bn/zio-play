115a64c485c361bb1d7d0c3f87b800c1c1e5f06a
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-04-05 09:10:19 -0400

    Another set of changes for Board, still there are compilation errors

--- src/main/scala/org/mypackage/domain/Board.scala
+++ src/main/scala/org/mypackage/domain/Board.scala
@@ -1,6 +1,6 @@
 package org.mypackage.domain
 
-import zio.UIO
+import zio._
 
 import scala.annotation.meta.field
 
@@ -26,5 +26,20 @@ object Board {
       Set(4, 5, 6),
       Set(7, 8, 9)
     )
+
+    val verticalWins = Set(
+      Set(1,4,7),
+      Set(2,5,8),
+      Set(3,6,9)
+    )
+
+    val diagonalWins = Set(
+      Set(1,5,9),
+      Set(3,5,7)
+    )
+
+    ZIO {
+      (horizontalWins ++ verticalWins ++ diagonalWins).map(_.map(i => Field.make(i.toString).get))
+    }.orDieWith(_ => new IllegalStateException)
   }
 }
