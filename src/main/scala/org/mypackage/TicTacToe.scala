512ac933f52f48857a4e7dc461e71c1181be748a
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-04-07 08:09:29 -0400

    Added the rest of the starter

--- src/main/scala/org/mypackage/TicTacToe.scala
+++ src/main/scala/org/mypackage/TicTacToe.scala
@@ -2,7 +2,7 @@ package org.mypackage
 
 import zio.console.{Console, putStrLn}
 import zio.random.Random
-import zio.{ExitCode, UIO, URIO, ZEnv}
+import zio.{ExitCode, UIO, URIO, ZEnv, ZIO}
 import org.mypackage.domain.*
 
 object TicTacToe extends App {
@@ -14,20 +14,24 @@ object TicTacToe extends App {
 
   def programLoop(state: State): URIO[Random with Console, Unit] = ???
 
-  def drawBoard(board: Board): URIO[Console, Unit] =
+  def drawBoard(board: Board): ZIO[Console, Exception, Unit] =
     putStrLn {
       Field.All
         .map(field => board.fields.get(field) -> field.value)
         .map {
           case (Some(piece: Piece), _) => piece.toString
-          case (None, value)           => value.toString
+          case (None, value) => value.toString
         }
         .sliding(3, 3)
-        .map(fields => s"""${fields.mkString(" || ")} """)
+        .map(fields => s""" ${fields.mkString(" || ")} """)
         .mkString("\n=======||========||======\n")
     }
 
-  def step(state: State.Ongoing): URIO[Random with Console, State] = ???
+  def step(state: State.Ongoing): URIO[Random with Console, State] =
+    for {
+      nextMove <- if (state.isComputerTurn) getComputerMove(state.board) else getPlayerMove(state.board)
+      nextState <- takeField(state, nextMove)
+    } yield nextState
 
   def getComputerMove(board: Board): URIO[Random with Console, Field] = ???
 
@@ -35,8 +39,19 @@ object TicTacToe extends App {
 
   def takeField(state: State.Ongoing, field: Field): URIO[Console, State] = ???
 
-  def getGameResult(board: Board): UIO[Option[GameResult]] = ???
+  def getGameResult(board: Board): UIO[Option[GameResult]] =
+    for {
+      crossWin <- isWinner(board, Piece.X)
+      noughtWin <- isWinner(board, Piece.O)
+      gameResult <- if (crossWin && noughtWin)
+        ZIO.die(new IllegalStateException("It should not be possible for both players to win!"))
+      else if (crossWin) UIO.succeed(GameResult.Win(Piece.X)).asSome
+      else if (noughtWin) UIO.succeed(GameResult.Win(Piece.O)).asSome
+      else if (board.isFull) UIO.succeed(GameResult.Draw).asSome
+      else UIO.none
+    } yield gameResult
 
-  def isWinner(board: Board, piece: Piece): UIO[Boolean] = ???
+  def isWinner(board: Board, piece: Piece): UIO[Boolean] =
+    Board.winnerCombinations.map(combinations => combinations.exists(_ subsetOf board.fieldsOccupiedByPiece(piece)))
 
 }
