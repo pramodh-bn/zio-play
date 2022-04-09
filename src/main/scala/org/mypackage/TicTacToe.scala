4c66ad67184fd7906aec128dce204d1a3f22a9ee
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-04-09 12:15:48 -0400

    Slowly working through the video stopped at 58:50

--- src/main/scala/org/mypackage/TicTacToe.scala
+++ src/main/scala/org/mypackage/TicTacToe.scala
@@ -1,20 +1,45 @@
 package org.mypackage
 
-import zio.console.{Console, putStrLn}
-import zio.random.Random
+import zio.console.{Console, getStrLn, putStr, putStrLn}
+import zio.random.{Random, nextBoolean}
 import zio.{ExitCode, UIO, URIO, ZEnv, ZIO}
 import org.mypackage.domain.*
 
-object TicTacToe extends App {
-  def run(args: List[String]): URIO[ZEnv, ExitCode] = ???
+object TicTacToe extends zio.App {
+  def run(args: List[String]): URIO[ZEnv, ExitCode] = choosePlayerPiece.exitCode
 
-  def choosePlayerPiece: URIO[Console, Piece] = ???
+  /*def choosePlayerPiece: URIO[Console, Piece] = This def can be converted to val because
+  since it is immutable, it will always evaulate to the same value
+     putStr("Do you want to be X or O ?").flatMap(_ => getStrLn.orDie)
+     * instead of flat map, we can use for comprehension
+     *
+     */
+    val choosePlayerPiece: URIO[Console, Piece] =
+    for {
+      /**_ <- putStr("Do you want to be X or O ?").orDie
+      input <- getStrLn.orDie
+       There is another way of handling sequencing in Zio and that is zip method.
+       this is apart from for, flatmap or map option
+       */
+      input <- putStr("Do you want to be X or O ?").orDie *> getStrLn.orDie // *> is equal to zipRight
 
-  def whichPieceGoesFirst: URIO[Random, Piece] = ???
+      /**piece <- ZIO.fromOption(Piece.make(input)).orElse(putStrLn("Invalid Input").flatMap(_ => choosePlayerPiece)).orDie
+       * orElse could be transformed to <> this is the power of syntactic sugar
+       * instead of using flatMap, can use zipRight method or *> zipRight always returns the value from the right operation
+      **/
+      piece <- ZIO.fromOption(Piece.make(input)) <> (putStrLn("Invalid Input")  *> choosePlayerPiece).orDie
+    } yield piece
+
+
+  val whichPieceGoesFirst: URIO[Random, Piece] = nextBoolean.map {
+        // Again making this method val from def as it is immutable
+    case true => Piece.X
+    case false => Piece.O
+  }
 
   def programLoop(state: State): URIO[Random with Console, Unit] = ???
 
-  def drawBoard(board: Board): ZIO[Console, Exception, Unit] =
+  def drawBoard(board: Board): URIO[Console, Unit] =
     putStrLn {
       Field.All
         // (x) -> y way of creating tuples in Scala
@@ -26,7 +51,7 @@ object TicTacToe extends App {
         .sliding(3, 3)
         .map(fields => s""" ${fields.mkString(" || ")} """)
         .mkString("\n=======||========||======\n")
-    }
+    }.orDie
 
   def step(state: State.Ongoing): URIO[Random with Console, State] =
     for {
